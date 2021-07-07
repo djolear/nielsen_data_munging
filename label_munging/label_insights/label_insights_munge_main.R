5562cc910aaa41de53d7679d86f29feac4405815
djolear <djolear@stanford.edu>
2021-07-07 13:49:25 -0700

    changing names

--- label_munging/label_insights/label_insights_munge_main.R
+++ label_munging/label_insights/label_insights_munge_main.R
@@ -1,28 +1,47 @@
+###################
+## Load Packages ##
+###################
+
 library(tidyverse)
 
-li_upc <-
-  read_csv("G:/My Drive/research/projects/niel/label_insights_data/label_insights_upc_raw_021721.csv")
+###############
+## Load Data ##
+###############
 
-source("G:/My Drive/research/projects/niel/label_insights_analysis/label_insights_servings_conversion.R")
+label_insights <-
+  read_csv("G:/My Drive/research/projects/niel/label_insights_data/label_insights_031721.csv")
 
-write_csv(li_upc_update, "G:/My Drive/research/projects/niel/label_insights_data/label_insights_upc_sc_021721.csv")
+
+########################
+## Run UPC Conversion ##
+########################
+
+source("G:/My Drive/research/projects/niel/nielsen_data_munging/label_munging/label_insights/label_insights_convert_upcs.R")
 
 
-li_upc_cal_only <-
-  li_upc %>% 
+#############################
+## Run Servings Conversion ##
+#############################
+
+source("G:/My Drive/research/projects/niel/nielsen_data_munging/label_munging/label_insights/label_insights_servings_conversion.R")
+
+
+################
+## Write Data ##
+################
+
+# Write data with all columns
+write_csv(label_insights_servings_converted, "G:/My Drive/research/projects/niel/label_insights_data/label_insights_servings_converted_031721.csv")
+
+# Select and write data with only calorie information
+label_insights_servings_converted_calories <-
+  label_insights_servings_converted %>% 
   dplyr::select(
     UPC,
+    upc_new,
     Calories
   )
 
-write_csv(li_upc_cal_only, "G:/My Drive/research/projects/niel/label_insights_data/label_insights_upc_raw_cal_only_021721.csv")
+write_csv(label_insights_servings_converted_calories, "G:/My Drive/research/projects/niel/label_insights_data/label_insights_servings_converted_calories_only_031721.csv")
 
 
-li_upc_sc_cal_only <-
-  li_upc_update %>% 
-  dplyr::select(
-    UPC,
-    calories_sc
-  )
-
-write_csv(li_upc_sc_cal_only, "G:/My Drive/research/projects/niel/label_insights_data/label_insights_upc_sc_cal_only_021721.csv")
